ce1bbeaf73f258e10dcd445edad14fb92874cc86
Виктор <vitya.tigr15@mail.ru>
2025-08-26 14:44:32 +0300

    Исправил ФЗ10(2)

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,36 +23,28 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        try {
-            validateUser(user);
-            user.setId(userIdCounter++);
-            users.add(user);
-            log.info("Создан пользователь: {}", user);
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
-        } catch (Exception e) {
-            log.warn("Ошибка при создании пользователя: {}", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Возвращаем 400 при ошибке валидации
-        }
+        validateUser(user);
+        user.setId(userIdCounter++);
+        users.add(user);
+        log.info("Создан пользователь: {}", user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
+
 
     @PutMapping
     public ResponseEntity<User> updateUser(@RequestBody User user) {
-        try {
-            validateUser(user);
-            for (int i = 0; i < users.size(); i++) {
-                if (users.get(i).getId().equals(user.getId())) {
-                    users.set(i, user);
-                    log.info("Обновлен пользователь: {}", user);
-                    return new ResponseEntity<>(user, HttpStatus.OK);
-                }
+        validateUser(user);
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId().equals(user.getId())) {
+                users.set(i, user);
+                log.info("Обновлен пользователь: {}", user);
+                return new ResponseEntity<>(user, HttpStatus.OK);
             }
-            log.warn("Пользователь с id {} не найден.", user.getId());
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Возвращаем 404, если не нашли
-        } catch (Exception e) {
-            log.warn("Ошибка при обновлении пользователя: {}", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Возвращаем 400 при ошибке валидации
         }
+        log.warn("Пользователь с id {} не найден.", user.getId());
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
