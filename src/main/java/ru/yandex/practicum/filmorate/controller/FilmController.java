f99bdeade8d82e874a5531542f4c6afcd4fc3e7e
Виктор <vitya.tigr15@mail.ru>
2025-08-26 15:31:20 +0300

    Исправил ФЗ10(3)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 
@@ -25,33 +26,47 @@ public class FilmController {
 
     @PostMapping
     public ResponseEntity<Film> createFilm(@RequestBody Film film) {
-        validateFilm(film);
-        film.setId(filmIdCounter++);
-        films.add(film);
-        log.info("Добавлен фильм: {}", film);
-        return new ResponseEntity<>(film, HttpStatus.CREATED);
+        try {
+            validateFilm(film); // Проверяем фильм на соответствие требованиям
+            film.setId(filmIdCounter++); // Присваиваем ID и увеличиваем счетчик
+            films.add(film); // Добавляем фильм в хранилище
+            log.info("Добавлен фильм: {}", film); // Логируем добавление
+            return new ResponseEntity<>(film, HttpStatus.CREATED); // Возвращаем ответ с кодом 201
+        } catch (ValidationException e) {
+            log.warn("Ошибка валидации при создании фильма: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Возвращаем 400 при ошибке валидации
+        } catch (Exception e) {
+            log.error("Непредвиденная ошибка при создании фильма", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // Возвращаем 500 при других ошибках
+        }
     }
-
 
     @PutMapping
     public ResponseEntity<Film> updateFilm(@RequestBody Film film) {
-        validateFilm(film);
-        for (int i = 0; i < films.size(); i++) {
-            if (films.get(i).getId().equals(film.getId())) {
-                films.set(i, film);
-                log.info("Обновлен фильм: {}", film);
-                return new ResponseEntity<>(film, HttpStatus.OK);
+        try {
+            validateFilm(film); // Проверяем фильм на соответствие требованиям
+            for (int i = 0; i < films.size(); i++) {
+                if (films.get(i).getId().equals(film.getId())) { // Ищем фильм по ID
+                    films.set(i, film); // Обновляем фильм
+                    log.info("Обновлен фильм: {}", film); // Логируем обновление
+                    return new ResponseEntity<>(film, HttpStatus.OK); // Возвращаем ответ с кодом 200
+                }
             }
+            log.warn("Фильм с id {} не найден.", film.getId());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Возвращаем 404, если фильм не найден
+        } catch (ValidationException e) {
+            log.warn("Ошибка валидации при обновлении фильма: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            log.error("Непредвиденная ошибка при обновлении фильма", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        log.warn("Фильм с id {} не найден.", film.getId());
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
 
     @GetMapping
     public ResponseEntity<List<Film>> getAllFilms() {
         log.info("Получен запрос на получение всех фильмов.");
-        return new ResponseEntity<>(films, HttpStatus.OK);
+        return new ResponseEntity<>(films, HttpStatus.OK); // Возвращаем список фильмов с кодом 200
     }
 }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.ArrayList;
@@ -23,28 +24,42 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        validateUser(user);
-        user.setId(userIdCounter++);
-        users.add(user);
-        log.info("Создан пользователь: {}", user);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        try {
+            validateUser(user); // Валидация пользователя
+            user.setId(userIdCounter++);
+            users.add(user);
+            log.info("Создан пользователь: {}", user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (ValidationException e) {
+            log.warn("Ошибка валидации при создании пользователя: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Возвращаем 400 при ошибке валидации
+        } catch (Exception e) {
+            log.error("Непредвиденная ошибка при создании пользователя", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // Возвращаем 500 при других ошибках
+        }
     }
-
 
     @PutMapping
     public ResponseEntity<User> updateUser(@RequestBody User user) {
-        validateUser(user);
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getId().equals(user.getId())) {
-                users.set(i, user);
-                log.info("Обновлен пользователь: {}", user);
-                return new ResponseEntity<>(user, HttpStatus.OK);
+        try {
+            validateUser(user);
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).getId().equals(user.getId())) {
+                    users.set(i, user);
+                    log.info("Обновлен пользователь: {}", user);
+                    return new ResponseEntity<>(user, HttpStatus.OK);
+                }
             }
+            log.warn("Пользователь с id {} не найден.", user.getId());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (ValidationException e) {
+            log.warn("Ошибка валидации при обновлении пользователя: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            log.error("Непредвиденная ошибка при обновлении пользователя", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        log.warn("Пользователь с id {} не найден.", user.getId());
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
