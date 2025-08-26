369c39c5b57ca0b96d4442fb0a408161316a3f4c
Виктор <vitya.tigr15@mail.ru>
2025-08-26 16:26:13 +0300

    13

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.validate.UserValidate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static ru.yandex.practicum.filmorate.validate.UserValidate.validateUser;
 
 @RestController
 @RequestMapping("/users")
@@ -23,28 +23,36 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        validateUser(user);
-        user.setId(userIdCounter++);
-        users.add(user);
-        log.info("Создан пользователь: {}", user);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        try {
+            UserValidate.validateUser(user); // Проверяем валидацию
+            user.setId(userIdCounter++);
+            users.add(user);
+            log.info("Создан пользователь: {}", user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED); // Возвращаем 201
+        } catch (Exception e) {
+            log.error("Ошибка при создании пользователя: {}", e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST); // Возвращаем код 400 при ошибке валидации
+        }
     }
-
 
     @PutMapping
     public ResponseEntity<User> updateUser(@RequestBody User user) {
-        validateUser(user);
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getId().equals(user.getId())) {
-                users.set(i, user);
-                log.info("Обновлен пользователь: {}", user);
-                return new ResponseEntity<>(user, HttpStatus.OK);
+        try {
+            UserValidate.validateUser(user); // Проверяем валидацию
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).getId().equals(user.getId())) {
+                    users.set(i, user);
+                    log.info("Обновлен пользователь: {}", user);
+                    return new ResponseEntity<>(user, HttpStatus.OK);
+                }
             }
+            log.warn("Пользователь с id {} не найден.", user.getId());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log.error("Ошибка при обновлении пользователя: {}", e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST); // Возвращаем код 400 при ошибке валидации
         }
-        log.warn("Пользователь с id {} не найден.", user.getId());
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
