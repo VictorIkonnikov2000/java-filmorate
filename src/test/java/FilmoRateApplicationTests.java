60bb59e3facd4a0763660c6c22de832050b83c1c
Виктор <vitya.tigr15@mail.ru>
2025-09-23 22:57:36 +0300

    ФЗ 12 первая версия8

--- src/test/java/FilmoRateApplicationTests.java
+++ src/test/java/FilmoRateApplicationTests.java
@@ -2,7 +2,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.model.User;
