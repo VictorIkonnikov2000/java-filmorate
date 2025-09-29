f5c41161a18ccf2e5eee7749b376ac07a5dfdcb6
Виктор <vitya.tigr15@mail.ru>
2025-09-29 11:35:06 +0300

    ФЗ12 исправленный 17

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,13 @@
-insert into mpa_ratings(name)
-values
-    ('G'),
-    ('PG'),
-    ('PG-13'),
-    ('R'),
-    ('NC-17');
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (1, 'G') ON CONFLICT DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (2, 'PG') ON CONFLICT DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (3, 'PG-13') ON CONFLICT DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (4, 'R') ON CONFLICT DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (5, 'NC-17') ON CONFLICT DO NOTHING;
 
-insert into genres(name)
-values
-    ('Комедия'),
-    ('Драма'),
-    ('Мультфильм'),
-    ('Триллер'),
-    ('Документальный'),
-    ('Боевик');
+-- Заполнение таблицы genres
+INSERT INTO genres (genre_id, name) VALUES (1, 'Комедия') ON CONFLICT DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (2, 'Драма') ON CONFLICT DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (3, 'Мультфильм') ON CONFLICT DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (4, 'Триллер') ON CONFLICT DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (5, 'Боевик') ON CONFLICT DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (6, 'Детектив') ON CONFLICT DO NOTHING;
