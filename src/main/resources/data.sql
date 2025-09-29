04c9c97684ab8dbb337436d0886ae23fe03e763a
Виктор <vitya.tigr15@mail.ru>
2025-09-29 12:33:11 +0300

    ФЗ12 исправленный 18

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
-INSERT INTO mpa_ratings (mpa_id, name) VALUES (1, 'G') ON CONFLICT DO NOTHING;
-INSERT INTO mpa_ratings (mpa_id, name) VALUES (2, 'PG') ON CONFLICT DO NOTHING;
-INSERT INTO mpa_ratings (mpa_id, name) VALUES (3, 'PG-13') ON CONFLICT DO NOTHING;
-INSERT INTO mpa_ratings (mpa_id, name) VALUES (4, 'R') ON CONFLICT DO NOTHING;
-INSERT INTO mpa_ratings (mpa_id, name) VALUES (5, 'NC-17') ON CONFLICT DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (1, 'G') ON CONFLICT (mpa_id) DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (2, 'PG') ON CONFLICT (mpa_id) DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (3, 'PG-13') ON CONFLICT (mpa_id) DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (4, 'R') ON CONFLICT (mpa_id) DO NOTHING;
+INSERT INTO mpa_ratings (mpa_id, name) VALUES (5, 'NC-17') ON CONFLICT (mpa_id) DO NOTHING;
 
--- Заполнение таблицы genres
-INSERT INTO genres (genre_id, name) VALUES (1, 'Комедия') ON CONFLICT DO NOTHING;
-INSERT INTO genres (genre_id, name) VALUES (2, 'Драма') ON CONFLICT DO NOTHING;
-INSERT INTO genres (genre_id, name) VALUES (3, 'Мультфильм') ON CONFLICT DO NOTHING;
-INSERT INTO genres (genre_id, name) VALUES (4, 'Триллер') ON CONFLICT DO NOTHING;
-INSERT INTO genres (genre_id, name) VALUES (5, 'Боевик') ON CONFLICT DO NOTHING;
-INSERT INTO genres (genre_id, name) VALUES (6, 'Детектив') ON CONFLICT DO NOTHING;
+-- Вставка начальных данных для жанров
+-- Эти данные также являются справочными и используются для классификации фильмов.
+INSERT INTO genres (genre_id, name) VALUES (1, 'Комедия') ON CONFLICT (genre_id) DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (2, 'Драма') ON CONFLICT (genre_id) DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (3, 'Мультфильм') ON CONFLICT (genre_id) DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (4, 'Триллер') ON CONFLICT (genre_id) DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (5, 'Документальный') ON CONFLICT (genre_id) DO NOTHING;
+INSERT INTO genres (genre_id, name) VALUES (6, 'Боевик') ON CONFLICT (genre_id) DO NOTHING;
