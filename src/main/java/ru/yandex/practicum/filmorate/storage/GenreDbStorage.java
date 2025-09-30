0d3f9bbfe5006cf8d44eea6f6c22eeef6da94eed
Виктор <vitya.tigr15@mail.ru>
2025-09-30 06:12:55 +0300

    ФЗ12 новый 22

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreDbStorage.java
@@ -17,7 +17,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 @Slf4j
 @Component("GenreDbStorage")
